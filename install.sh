b8b8b30ef6015d1cb6d64839e780313a2b318ad8
Marcookl <marcoklunder@outlook.com>
2020-07-08 20:00:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -562,7 +562,7 @@ install_pterodactyl() {
     output "Downloading Pterodactyl..."
     mkdir -p /var/www/pterodactyl
     cd /var/www/pterodactyl
-    curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/download/v0.7.17/panel.tar.gz
+    curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/download/v0.7.16/panel.tar.gz
     tar --strip-components=1 -xzvf panel.tar.gz
     chmod -R 755 storage/* bootstrap/cache/
 
